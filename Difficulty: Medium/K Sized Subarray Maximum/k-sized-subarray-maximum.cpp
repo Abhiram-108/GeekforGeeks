41fba8e70a808951c2aeced20405b99b5b09f5f7
Abhiram Pedada <149411236+Abhiram-108@users.noreply.github.com>
2025-06-08 22:07:43 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/K Sized Subarray Maximum/k-sized-subarray-maximum.cpp
+++ Difficulty: Medium/K Sized Subarray Maximum/k-sized-subarray-maximum.cpp
@@ -1,66 +1,23 @@
-//{ Driver Code Starts
-#include <bits/stdc++.h>
-using namespace std;
-
-
-// } Driver Code Ends
-
 class Solution {
   public:
-    vector<int> maxOfSubarrays(vector<int>& a, int k) {
+    vector<int> maxOfSubarrays(vector<int>&a, int k) {
         // code here
         deque<int>dq;
-        vector<int>ans;
         int n=a.size();
+        vector<int>ans;
         for(int i=0;i<n;i++){
-            if(dq.size() && dq.front()<=i-k){
-                dq.pop_front();
-            }
-            while(dq.size() && a[dq.back()]<=a[i]){
-                dq.pop_back();
-            }
+            
+            if(dq.size() && dq.front()<=i-k)
+            dq.pop_front();
+            
+            while(dq.size() && a[dq.back()]<=a[i])
+            dq.pop_back();
+            
             dq.push_back(i);
             if(i>=k-1){
-            ans.push_back(a[dq.front()]);
+                ans.push_back(a[dq.front()]);
             }
         }
         return ans;
     }
 };
-
-
-//{ Driver Code Starts.
-
-int main() {
-    int t;
-    cin >> t;
-    cin.ignore(); // Ignore newline character after t
-
-    while (t--) {
-        vector<int> arr;
-        int k;
-        string inputLine;
-
-        getline(cin, inputLine); // Read the array input as a line
-        stringstream ss(inputLine);
-        int value;
-        while (ss >> value) {
-            arr.push_back(value);
-        }
-
-        cin >> k;
-        cin.ignore(); // Ignore newline character after k input
-
-        Solution obj;
-        vector<int> res = obj.maxOfSubarrays(arr, k);
-        for (int i = 0; i < res.size(); i++)
-            cout << res[i] << " ";
-        cout << endl;
-        cout << "~"
-             << "\n";
-    }
-
-    return 0;
-}
-
-// } Driver Code Ends
