ef5c4a6f3d4817e256e0c7725fadc32525dd5a6a
Abhiram Pedada <149411236+Abhiram-108@users.noreply.github.com>
2025-06-14 10:16:57 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Easy/Product array puzzle/product-array-puzzle.cpp
+++ Difficulty: Easy/Product array puzzle/product-array-puzzle.cpp
@@ -1,9 +1,3 @@
-//{ Driver Code Starts
-#include <bits/stdc++.h>
-using namespace std;
-
-
-// } Driver Code Ends
 // User function template for C++
 
 class Solution {
@@ -11,50 +5,18 @@ class Solution {
     vector<int> productExceptSelf(vector<int>& arr) {
         // code here
         int n=arr.size();
-        vector<int>left(n,1);
-        vector<int>right(n,1);
+        vector<int>l(n,1);
+        vector<int>r(n,1);
+        vector<int>ans(n,1);
         for(int i=1;i<n;i++)
-        left[i]=left[i-1]*arr[i-1];
+        l[i]=l[i-1]*arr[i-1];
         for(int i=n-2;i>=0;i--)
-        right[i]=right[i+1]*arr[i+1];
-        for(int i=0;i<n;i++){
-            arr[i]=left[i]*right[i];
-        }
-        return arr;
+        r[i]=r[i+1]*arr[i+1];
+        for(int i=0;i<n;i++)
+        ans[i]=l[i]*r[i];
+        
+        return ans;
+        
+        
     }
 };
-
-
-//{ Driver Code Starts.
-
-int main() {
-
-    int t;
-    cin >> t;
-    cin.ignore();
-
-    while (t--) {
-        vector<int> arr;
-        string input;
-        getline(cin, input);
-        stringstream ss(input);
-        int number;
-
-        while (ss >> number) {
-            arr.push_back(number);
-        }
-
-        Solution obj;
-        vector<int> res = obj.productExceptSelf(arr);
-
-        for (int i = 0; i < res.size(); i++) {
-            cout << res[i] << " ";
-        }
-        cout << endl;
-        cout << "~\n";
-    }
-
-    return 0;
-}
-
-// } Driver Code Ends
