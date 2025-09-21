02b8ba5b68bcc7e83a681d775161062a38248b0f
Abhiram Pedada <149411236+Abhiram-108@users.noreply.github.com>
2025-09-21 15:07:19 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Easy/N meetings in one room/n-meetings-in-one-room.cpp
+++ Difficulty: Easy/N meetings in one room/n-meetings-in-one-room.cpp
@@ -1,35 +1,27 @@
 class Solution {
+    static bool cmp(pair<int,int>& a, pair<int,int>& b) {
+        return a.second < b.second; // sort by end time
+    }
 public:
     int maxMeetings(vector<int>& start, vector<int>& end) {
-        struct Meeting {
-            int start;
-            int end;
-        };
-        
-        // Custom comparator to sort meetings by end time
-        auto cmp = [](const Meeting& a, const Meeting& b) {
-            return a.end < b.end;
-        };
-
+        vector<pair<int,int>> a;
         int n = start.size();
-        vector<Meeting> meetings(n);
-        
-        for (int i = 0; i < n; ++i) {
-            meetings[i] = {start[i], end[i]};
+        for (int i = 0; i < n; i++) {
+            a.push_back({start[i], end[i]});
         }
-
-        sort(meetings.begin(), meetings.end(), cmp);
-
-        int count = 1;
-        int lastEnd = meetings[0].end;
         
-        for (int i = 1; i < n; ++i) {
-            if (meetings[i].start > lastEnd) {
-                ++count;
-                lastEnd = meetings[i].end;
+        sort(a.begin(), a.end(), cmp);
+        
+        int ct = 1; 
+        auto it1 = a[0]; // take first meeting
+        
+        for (int i = 1; i < n; i++) {
+            auto it2 = a[i];
+            if (it2.first > it1.second) { // no overlap
+                ct++;
+                it1 = it2;
             }
         }
-
-        return count;
+        return ct;
     }
 };
